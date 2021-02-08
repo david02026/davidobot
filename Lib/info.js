ef2bb2951450944e5acbc37c40cfddb55606618d
david02026 <78765944+david02026@users.noreply.github.com>
2021-02-08 20:03:59 -0300

    Add files via upload

---
+++ Lib/info.js
@@ -0,0 +1,44 @@
+exports.info = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT ATIVO: *${aktif}*
+
+         
+╔════════════════════
+║ *Feito por: $Davidzin*
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*OS CRÉDITOS NAO SÃO MEUS*
+║*APENAS FIZ UMA VERSÃO INSTÁVEL* vsfd se n gostar.
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│➸ *AUTOR*: David
+║│➸ *DESIGNER*: David
+║│➸ *YOUTUBE*: off
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Faça login aqui😙*
+║│ _${whatsapp}_
+║│➸ *YouTube*
+║│ _${nomer}_
+║│➸ *Instagram <Segue>*
+║│ _${instagram}_
+║│➸ *Criador*
+║│    David ツ
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*Feito por Davidzin ツ*_
+╚════════════════════`
+}
